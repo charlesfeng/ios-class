9ee495d14436abf2ba0a61d63cacdf505260a2b0
Charles Feng <charles@charles.io>
2014-03-05 23:13:48 -0800

    lecture 13; photomania

---
+++ Photomania/Photomania/PhotomaniaViewController.m
@@ -0,0 +1,29 @@
+//
+//  PhotomaniaViewController.m
+//  Photomania
+//
+//  Created by Charles Feng on 3/4/14.
+//  Copyright (c) 2014 Charles Feng. All rights reserved.
+//
+
+#import "PhotomaniaViewController.h"
+
+@interface PhotomaniaViewController ()
+
+@end
+
+@implementation PhotomaniaViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Photomania/Photomania/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photomania
+//
+//  Created by Charles Feng on 3/4/14.
+//  Copyright (c) 2014 Charles Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PhotomaniaAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PhotomaniaAppDelegate class]));
+    }
+}
